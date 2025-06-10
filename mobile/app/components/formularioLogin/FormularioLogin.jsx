1bae213f97b04416d678cd4a8375f55bcad8afe3
Lucas Duarte <169089232+Lduarte123@users.noreply.github.com>
2025-06-10 20:41:58 -0300

    yi
    jhhjhj

--- mobile/app/components/formularioLogin/FormularioLogin.jsx
+++ mobile/app/components/formularioLogin/FormularioLogin.jsx
@@ -13,7 +13,8 @@ export default function FormularioLogin() {
       Alert.alert("Erro", "Preencha todos os campos");
       return;
     }
-// hgjkl
+// hgjklyyy
+// hgjklyyy
     if (email === "leo@gmail.com" && senha === "123") {
       Alert.alert("Sucesso", "Bem-vindes todes");
       setTimeout(() => {
