3979b41b4a3bf3c4f6c4e2a6cea9fe80bb7047fa
cauaenzo <cauaenzo07@gmail.com>
2025-06-16 20:00:06 -0300

    :construction::dbInit

--- nsql/server.js
+++ nsql/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const rainRoutes = require('./routes/rainRoutes');
+require('./config/dbInit');
 
 dotenv.config();
 
