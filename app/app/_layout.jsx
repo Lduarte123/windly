f7f1aaa49fee4589fe0d727ddf164d350f686491
Victor <victorconta1009@gmail.com>
2025-07-04 12:09:53 -0300

    fix buttons layout

--- app/app/_layout.jsx
+++ app/app/_layout.jsx
@@ -4,12 +4,15 @@ import { TouchableOpacity, View } from 'react-native';
 import { ThemeProvider, useTheme } from '../components/ThemeContext';
 import { AuthProvider } from '../components/authContext/AuthContext';
 import { ConfigProvider } from '../components/configContext';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 
 function ThemedTabs() {
   const { dark } = useTheme();
 
   const backgroundColor = dark ? '#151718' : '#fff';
   const textColor = dark ? '#ECEDEE' : '#11181C';
+  const insets = useSafeAreaInsets();
 
   return (
     <View style={{ flex: 1, backgroundColor }}>
@@ -28,7 +31,7 @@ function ThemedTabs() {
             borderTopEndRadius: 20,
             borderTopStartRadius: 20,
             height: 80,
-            paddingBottom: 10,
+            paddingBottom: 25 + insets,
             paddingTop: 10,
             shadowColor: '#000',
             shadowOffset: { width: 0, height: 5 },
